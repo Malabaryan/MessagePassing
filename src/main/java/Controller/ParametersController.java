4fba74993a8445776df5b4a6670b91de825cc6b0
Kenneth Herrera <kfhv.24@gmail.com>
2020-06-15 11:07:10 -0600

    Changed ParametersController methods to static.

--- src/main/java/Controller/ParametersController.java
+++ src/main/java/Controller/ParametersController.java
@@ -15,25 +15,25 @@ public  class ParametersController {
     private static ParameterState format;
     private static ParameterState queueStrategy;
 
-    public ParameterState getSyncronization() {
+    public static ParameterState getSyncronization() {
         if (syncronization == null)
             syncronization = ParameterState.Sync_Send_Blocking;
         return syncronization;
     }
 
-    public ParameterState getAddressing() {
+    public static ParameterState getAddressing() {
         if (addressing == null)
             addressing = ParameterState.Addr_Direct_Send;
         return addressing;
     }
 
-    public ParameterState getFormat() {
+    public static ParameterState getFormat() {
         if (format == null)
             format = ParameterState.Form_Content;
         return format;
     }
 
-    public ParameterState getQueueStrategy() {
+    public static ParameterState getQueueStrategy() {
         if (queueStrategy == null)
             queueStrategy = ParameterState.Queue_FIFO;
         return queueStrategy;
